d97fa7263f2259c835a5d3174a85037120409dd8
Thiago Augusto Costa Flausino <thiago.flausino@totvs.com.br>
2020-08-04 18:23:35 -0300

    update db

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -107,8 +107,8 @@ function CadastroCategoria() {
 
       <ul>
         {categorias.map((categoria) => (
-          <li key={`${categoria.nome}`}>
-            {categoria.nome}
+          <li key={`${categoria.titulo}`}>
+            {categoria.titulo}
           </li>
         ))}
       </ul>
